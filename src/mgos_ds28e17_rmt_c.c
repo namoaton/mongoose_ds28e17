0aa72565b028c29e5f8c54d810e2a2aa48ef9b5c
Namo Aton <namoaton@gmail.com>
2019-09-24 10:43:21 +0300

    mod init func

--- src/mgos_ds28e17_rmt_c.c
+++ src/mgos_ds28e17_rmt_c.c
@@ -3,7 +3,7 @@
 #include "mgos_ds28e17_rmt.h"
 #include "mgos_onewire_rmt.h"
 
-bool mgos_ds28e17_rmt_init(void) {
+bool mgos_ds28e17_init(void) {
   LOG(LL_WARN, (__FUNCTION__));
   return true;
 }
