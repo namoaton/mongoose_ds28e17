1aef963efaf9e25bc796815ae9784315496f0928
Namo Aton <namoaton@gmail.com>
2019-09-26 08:38:48 +0300

    1

--- src/DS28E17Rmt.cpp
+++ src/DS28E17Rmt.cpp
@@ -155,7 +155,6 @@ bool  DS28E17Rmt::WriteDataStop(uint8_t* deviceAddress, uint8_t i2c_addr, uint8_
     uint8_t command[len + 5] = {Write_Data_Stop, i2c_addr, len};
     memcpy(command+3,data,len );
     uint16_t  crc = crc16(command, len+3);
-    for(int m=0; m<len+5; m++)
     command[len+3] = crc & 0xff;
     command[len+4] = crc >>8;
     for(int m=0; m<len+5; m++){
