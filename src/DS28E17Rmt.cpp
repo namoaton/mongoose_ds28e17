cffadbd0517be96d8df220166361d1f3070d16b2
Namo Aton <namoaton@gmail.com>
2019-10-01 13:42:06 +0300

    test

--- src/DS28E17Rmt.cpp
+++ src/DS28E17Rmt.cpp
@@ -86,8 +86,8 @@ bool  DS28E17Rmt::ow_read_bytes(uint8_t* deviceAddress, uint8_t *command, uint8_
     if (b == 0) return false;
     _ow->select(deviceAddress);
     _ow->write_bytes(command,len_w + 2);
-    mgos_msleep(50);
-    _ow->read_bytes(status, 1);
+    mgos_msleep(10);
+//    _ow->read_bytes(status, 1);
     uint8_t read_bytes[len_r+1];
     _ow->read_bytes(read_bytes, len_r+1);
     b = _ow->reset();
